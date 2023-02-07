c516de2b53352e5a757738bdb36e666cf4033218
M1TRO11 <mitrofanov.g.o@inbox.ru>
2023-02-07 19:50:37 +0300

    Removed test comments

--- src/main/java/org/tct/onminelessons/item/ModItems.java
+++ src/main/java/org/tct/onminelessons/item/ModItems.java
@@ -10,13 +10,11 @@ import net.minecraft.util.Identifier;
 import org.tct.onminelessons.OnMineLessons;
 
 public class ModItems {
-    public static final Item NEW_ITEM = registerItem("new_item", new Item(new FabricItemSettings()), ModItemGroups.NEW_GROUP); //test change (this is items group xD)
+    public static final Item NEW_ITEM = registerItem("new_item", new Item(new FabricItemSettings()), ModItemGroups.NEW_GROUP);
     private static Item registerItem(String id, Item item) { return Registry.register(Registries.ITEM, new Identifier(OnMineLessons.MODID, id), item); }
     private static Item registerItem(String id, Item item, ItemGroup itemGroup) {
         Item returnItem = Registry.register(Registries.ITEM, new Identifier(OnMineLessons.MODID, id), item);
         ItemGroupEvents.modifyEntriesEvent(itemGroup).register(entries -> entries.add(returnItem));
-
-        //wtferhgiehgierhg
 
         return returnItem;
     }
